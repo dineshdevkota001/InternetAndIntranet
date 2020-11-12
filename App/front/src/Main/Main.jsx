30b831e61302c119a378caa2d2e44302f8272863
wmaharsh <wmaharsh@gmail.com>
2020-11-12 14:05:23 +0545

    Added TestObject
    Changed workflow from native ThreeJS to React-three-fiber container

--- App/front/src/Main/Main.jsx
+++ App/front/src/Main/Main.jsx
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import Details from './Details/Details'
-import ThreeViewer from './ThreeViewer'
 import './three.css'
+import ThreeContainer from './ThreeContainer';
 class Main extends Component{
     render(){
         return (
             <div id='Main' className='row m-1 vh-90 justify-content-center verticen' >
                 <Details/>
-                <ThreeViewer />
+                <ThreeContainer />
             </div>
         );
     }
---
+++ App/front/src/Main/ThreeContainer.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class ThreeContainer extends Component{
+    render () {
+        return (
+        <div className='col verticen'>
+            <div className='m-2 p-5 h-100  border rounded shadow-lg' id = 'three-main'>
+            </div>
+        </div>
+        )
+    }
+}
