35655adbf944a541ae583b0a3969ab80d0bc82ad
wmaharsh <wmaharsh@gmail.com>
2020-11-12 16:33:49 +0545

    Janky Height Fix
    Removed verticen to somewhat fix the canvas height, albeit jankily.
    Also added some ambient light to 3D space and a sphere for no good reason.

--- App/front/src/Main/Main.jsx
+++ App/front/src/Main/Main.jsx
@@ -5,9 +5,11 @@ import ThreeContainer from './ThreeContainer';
 class Main extends Component{
     render(){
         return (
-            <div id='Main' className='row m-1 vh-90 justify-content-center verticen' >
-                <Details/>
-                <ThreeContainer />
+            <div className = 'container-fluid'>
+                <div id='Main' className='row m-1 vh-90 fill-grow-1 justify-content-center verticen' >
+                    <Details/>
+                    <ThreeContainer />
+                </div>
             </div>
         );
     }
